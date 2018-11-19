9f23e7d8103ffcaa9d9a47b1bd3bd5f9cdb22141
Tomasmed18 <tomy-medina@hotmail.com>
2018-11-19 13:09:21 -0300

    Add files via upload

--- src/resources/AccionSemantica2.java
+++ src/resources/AccionSemantica2.java
@@ -5,20 +5,24 @@ public class AccionSemantica2 implements AccionSemantica{
 	//Esta accion semantica se encarga de guardar en la TS las cadenas de caracteres
 	
 	public Token ejecutar(AnalizadorLexico AL, char nuevoChar) {
-		String cadena = AL.buffer;
-		String claveTS = "'" + AL.buffer + "'"; 
-		
-		if(!AL.TS.containsKey(claveTS)) {
-			Atributos atts = new Atributos();
-			atts.set("Token", "CADENA_CARACTERES");
-			atts.set("Lexema", cadena);
-			atts.set("Tipo", "cadena_caracteres");
-			AL.altaEnTablaSimbolos(AL.buffer, atts);
+		if (AL.buffer.isEmpty())
+			AL.agregarError("Error: no se pueden definir cadenas de caracteres vacias vacias. Linea: " + AL.nroLinea);
+		else{
+			String cadena = AL.buffer;
+			String claveTS = "'" + AL.buffer + "'"; 
+			
+			if(!AL.TS.containsKey(claveTS)) {
+				Atributos atts = new Atributos();
+				atts.set("Token", "CADENA_CARACTERES");
+				atts.set("Lexema", cadena);
+				atts.set("Tipo", "cadena_caracteres");
+				AL.altaEnTablaSimbolos(AL.buffer, atts);
+			}
+			
+			
+			return new Token(Token.CADENA_CARACTERES, claveTS, AL.nroLinea);
 		}
-		
-		
-		return new Token(Token.CADENA_CARACTERES, claveTS, AL.nroLinea);
-		
+		return new Token(Token.YYERRCODE,null, AL.nroLinea);
 	}
 	
 }
